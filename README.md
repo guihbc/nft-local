370c8625d70a012e59e4d8d4671e35d08e36985c
Guilherme Henrique <guihchavesb@gmail.com>
2024-12-08 18:38:40 -0300

    add image to use as NFT content

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # NFT Local
 
-Compile the contract
+### Compile the contract
 
 ```shell
 ./scripts/build-contract.sh
+```
+
+### Deploy the contract
+
+```shell
+source venv/bin/activate
+pip install -r requirements.txt
+python ./scripts/deploy_contract.py
+```
+
+### Send image to ipfs
+
+```shell
+curl -X POST -F file=@./nft/sun.jpg "http://localhost:5001/api/v0/add"
+```
+
+Access file: http://localhost:8080/ipfs/{hash}
+
+### Send metadata to ipfs
+
+```shell
+curl -X POST -F file=@./nft/sun_metadata.json "http://localhost:5001/api/v0/add"
+```
+
+Access file: http://localhost:8080/ipfs/{hash}
+
+### Solidity mint function call
+
+```shell
+python nft.py
 ```
--- nft.py
+++ nft.py
@@ -9,12 +9,13 @@ with open("./contracts/MyNFT.abi", "r") as abi_file:
     ABI = abi_file.read()
 
 account_address = w3.eth.accounts[0] # Geth dev account
-contract_address = '0x4f1A982B5E6A1B3dc6484C6f695827f0B0a65473' # Deployed using ./scripts/deploy_contract.py
+contract_address = '0x1C2aC49C97658Cd5E970418Ca94750ab504C8654' # Deployed using ./scripts/deploy_contract.py
 
 print(f'Using geth default account: {account_address}')
 MyNFT = w3.eth.contract(address=contract_address, abi=ABI)
 
-mint_tx = MyNFT.functions.mint(to=account_address, metadataURI='http://localhost/test').build_transaction({
+nft_metadata = 'http://localhost:8080/ipfs/QmPowR52BtFdReHvWfpeUqm6oHtdoVydyE14wWo9hHNDU1'
+mint_tx = MyNFT.functions.mint(to=account_address, metadataURI=nft_metadata).build_transaction({
     'chainId': 1337,
     'from': account_address,
     'gas': 5000000,
@@ -25,3 +26,10 @@ mint_tx = MyNFT.functions.mint(to=account_address, metadataURI='http://localhost
 tx_hash = w3.eth.send_transaction(mint_tx)
 receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f"transaction completed! Hash: {receipt.transactionHash.hex()}")
+
+current_token_id = MyNFT.functions.nextTokenId().call() - 1
+owner_address = MyNFT.functions.ownerOf(current_token_id).call()
+print(f"The token {current_token_id} belongs to {owner_address}")
+
+token_uri = MyNFT.functions.tokenURI(current_token_id).call()
+print(f'Token URI: {token_uri}')
